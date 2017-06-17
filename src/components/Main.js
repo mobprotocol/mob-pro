917dcb1ed76d4b90305315f6fac376ed88017df8
Sean Pollock <seanpollock3344@gmail.com>
2017-06-17 14:57:41 -0400

    Grid layout for header content sections.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
+import Header from './header/Header'
+import Content from './content/Content'
+
 export default class Main extends Component {
   render() {
     return (
       <div>
-        mob
+        <Header />
+        <Content />
       </div>
     )
   }
