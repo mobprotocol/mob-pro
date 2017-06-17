5a1c3f3f6e78b24d04d859812e9f6daf0f8373d3
Sean Pollock <seanpollock3344@gmail.com>
2017-06-17 15:17:24 -0400

    Adding orderbook column.

---
+++ src/components/content/orderbook/Orderbook.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+const orderBookStyle = {
+  backgroundColor: '#29353d',
+  color: '#FFF',
+  height: '100%',
+  width: '100%'
+}
+
+export default class Orderbook extends Component {
+  render() {
+    return (
+      <div style={orderBookStyle}>
+
+      </div>
+    )
+  }
+}
