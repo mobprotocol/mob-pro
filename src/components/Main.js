c9c876d6c4ef1144164ef9a565b3e1affc12ccb7
Sean Pollock <seanpollock3344@gmail.com>
2017-06-20 11:55:06 -0400

    Wrapping react entry point in provider store

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 
 import Header from './header/Header'
 import Content from './content/Content'
@@ -6,10 +7,12 @@ import Content from './content/Content'
 export default class Main extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Content />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header />
+          <Content />
+        </div>
+      </Provider>
     )
   }
 }
