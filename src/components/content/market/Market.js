b3a12c5274b9e648c731c6b580d38fb3653c84ec
Sean Pollock <seanpollock3344@gmail.com>
2017-06-20 13:57:12 -0400

    Styling graph navigator

--- src/components/content/market/Market.js
+++ src/components/content/market/Market.js
@@ -5,22 +5,21 @@ import MarketDepth from './MarketDepth'
 const marketSyle = {
   width: '100%',
   height: '100%',
-  marginRight: 20
 }
 
 export default class Market extends Component {
   render() {
     return (
       <div style={marketSyle}>
-        <div style={{ height: 40, backgroundColor: '#9ca1a6' }}>
+        <div style={{ height: 60, backgroundColor: '#9ca1a6', width: '100%' }}>
           <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h4>Price</h4>
+            <h2>Price</h2>
           </div>
           <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h4>Depth</h4>
+            <h2>Depth</h2>
           </div>
           <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h4>Volume</h4>
+            <h2>Volume</h2>
           </div>
         </div>
         <PriceHistory />
