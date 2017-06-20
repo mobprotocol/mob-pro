ff143c8bcf59bfe94059cb90ec39bbe368ce82a3
Sean Pollock <seanpollock3344@gmail.com>
2017-06-20 14:49:47 -0400

    Adding react router to graph tab navigation

--- src/components/content/market/Market.js
+++ src/components/content/market/Market.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PriceHistory from './PriceHistory'
 import MarketDepth from './MarketDepth'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 const marketSyle = {
   width: '100%',
   height: '100%',
@@ -10,22 +10,26 @@ const marketSyle = {
 export default class Market extends Component {
   render() {
     return (
-      <div style={marketSyle}>
-        <div style={{ height: 60, backgroundColor: '#9ca1a6', width: '100%' }}>
-          <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h2>Price</h2>
+      <Router>
+        <div style={marketSyle}>
+          <div style={{ height: 60, backgroundColor: '#9ca1a6', width: '100%' }}>
+            <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
+              <h2>Price</h2>
+            </div>
+            <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
+              <h2>Depth</h2>
+            </div>
+            <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
+              <h2>Volume</h2>
+            </div>
           </div>
-          <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h2>Depth</h2>
-          </div>
-          <div style={{ width: '33%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h2>Volume</h2>
+          <div>
+            <Route path="/price" component={PriceHistory} />
+            <Route path="/volume" component={Volume} />
+            <Route path="/depth" component={MarketDepth} />
           </div>
         </div>
-        <div>
-          <PriceHistory />
-        </div>
-      </div>
+      </Router>
     )
   }
 }
