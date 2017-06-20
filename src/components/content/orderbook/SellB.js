e4fde28f3982942bbb0d7856a1798cf6d48e42ad
Sean Pollock <seanpollock3344@gmail.com>
2017-06-20 13:44:43 -0400

    Adding mock data and sell data for sellB book

--- src/components/content/orderbook/SellB.js
+++ src/components/content/orderbook/SellB.js
@@ -6,7 +6,75 @@ const sellBStyle = {
   color: '#FFF'
 }
 
+const data = [
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },
+  {
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  },{
+    rank: 1,
+    price: .95,
+    quantity: 55,
+    time: 1234134
+  }
+]
+
 export default class SellB extends Component {
+  sellBdata() {
+    return data.map((datum, i) => {
+      return (
+        <tr key={i}>
+          <th>{datum.rank}</th>
+          <th>{datum.price}</th>
+          <th>{datum.quantity}</th>
+          <th>{datum.time}</th>
+        </tr>
+      )
+    })
+  }
   render() {
     return (
       <div style={{ height: '50%', paddingTop: 15, overflow: 'scroll'}}>
@@ -20,6 +88,7 @@ export default class SellB extends Component {
             </tr>
           </thead>
           <tbody>
+            {this.sellBdata()}
           </tbody>
         </Table>
       </div>
