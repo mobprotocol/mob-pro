692faa4c45354b27fde3079fcab8621491dc8230
Sean Pollock <seanpollock3344@gmail.com>
2017-06-20 11:46:28 -0400

    Adding reducer for balance state.

---
+++ src/reducers/balance.js
@@ -0,0 +1,9 @@
+export default function balance(state = {}, action) {
+  switch(action.type) {
+    case 'UPDATE_BALANCE':
+      return {
+        ...state,
+        [action.asset]: action.data
+      }
+  }
+}
