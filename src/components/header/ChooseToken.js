531d2b5458a6b2ec0e109982aa47b3079c1fdd60
Sean Pollock <seanpollock3344@gmail.com>
2017-07-11 10:02:32 -0400

    mapping through tokens

--- src/components/header/ChooseToken.js
+++ src/components/header/ChooseToken.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react'
 import {
   Modal,
 } from 'react-bootstrap'
+import tokens from './tokens.json'
 
 export default class ChooseToken extends Component {
   constructor() {
     super()
   }
 
+  tokens() {
+    console.log('tokens', tokens)
+    return Object.keys(tokens).map((token) => {
+      return (
+        <div style={{ width: 150, height: 75}}>
+          token A
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
-      <Modal>
-        <h1>choose token</h1>
-      </Modal>
+      <div>
+        {this.tokens()}
+      </div>
     )
   }
 }
