050f243187c82fe6c74b9b557fb95ced0d09d07b
Sean Pollock <seanpollock3344@gmail.com>
2017-07-11 15:35:02 -0400

    restyling command panels

--- src/components/content/commands/Balance.js
+++ src/components/content/commands/Balance.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 const balanceStyle = {
   backgroundColor: '#b0b4b7',
   height: '50%',
+  fontWeight: 100
 }
 
 class Balance extends Component {
@@ -14,36 +15,38 @@ class Balance extends Component {
       tokenA: 'USD',
       tokenB: 'ETH',
       tokenAamount: 10.00000000,
-      tokenBamount:  10.00000000
+      tokenBamount:  10.00000000,
+      fontWeight: 100
     }
   }
 
   render() {
+    console.log('this.props', this.props)
     return (
       <Row style={balanceStyle}>
         <div style={{ height: '25%'}}>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>{this.state.tokenA}</h1>
+            <h1 style={{ fontWeight: 100}}>{this.props.tokenA}</h1>
           </div>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center'}}>
-            <h1>{this.state.tokenAamount}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.state.tokenAamount}</h1>
           </div>
         </div>
         <div style={{ height: '25%'}}>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }} >
-            <h1>{this.state.tokenB}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.props.tokenB}</h1>
           </div>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>{this.state.tokenBamount}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.state.tokenBamount}</h1>
           </div>
         </div>
         <div style={{ height: '25%', textAlign: 'center' }}>
-          <div style={{ backgroundColor: '#3a444d', color: '#FFF', display: 'inline-block', width: '75%' }}>
+          <div style={{ backgroundColor: '#3a444d', color: '#FFF', display: 'inline-block', width: '75%', borderRadius: 7 }}>
             <h2>Deposit</h2>
           </div>
         </div>
         <div style={{ height: '25%', textAlign: 'center' }}>
-          <div style={{ backgroundColor: '#3a444d', color: '#FFF', display: 'inline-block', width: '75%'}}>
+          <div style={{ backgroundColor: '#3a444d', color: '#FFF', display: 'inline-block', width: '75%', borderRadius: 7}}>
             <h2>Withdraw</h2>
           </div>
         </div>
@@ -53,7 +56,7 @@ class Balance extends Component {
 }
 
 const mapStoreToProps = (store) => {
-  return store.tokens
+  return store.token
 }
 
 export default connect(mapStoreToProps)(Balance)
--- src/components/content/commands/Trade.js
+++ src/components/content/commands/Trade.js
@@ -6,14 +6,15 @@ import Slider from 'react-rangeslider'
 
 const tradeStyle = {
   height: '50%',
-  backgroundColor: '#9ca1a6'
+  backgroundColor: '#9ca1a6',
+  marginLeft: 0 
 }
 export default class Trade extends Component {
   constructor() {
     super()
     this.state = {
       marketPrice: '1.450000',
-      quantity: '100',
+      quantity: 10,
       sendAmount: '30',
       receiveAmount: '45'
     }
@@ -33,16 +34,16 @@ export default class Trade extends Component {
           <div style={{ width: '33%', float: 'left' }}></div>
         </div>
       */}
-        <div style={{ height: '25%'}}>
+        <div style={{ height: '20%'}}>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>Price</h1>
+            <h1 style={{ fontWeight: 100 }}>Price</h1>
           </div>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center'}}>
-            <h1>{this.state.marketPrice}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.state.marketPrice}</h1>
           </div>
         </div>
-        <div style={{ height: '25%' }}>
-          <div style={{ paddingLeft: 20, paddingRight: 10 }}>
+        <div style={{ height: '20%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <div style={{ width: '90%' }}>
             <Slider
               value={this.state.quantity}
               oriantation="horizontal"
@@ -50,20 +51,20 @@ export default class Trade extends Component {
             />
           </div>
         </div>
-        <div style={{ height: '25%' }}>
+        <div style={{ height: '20%' }}>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>Send</h1>
+            <h1 style={{ fontWeight: 100 }}>Send</h1>
           </div>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>{this.state.sendAmount}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.state.sendAmount}</h1>
           </div>
         </div>
-        <div style={{ height: '25%' }}>
+        <div style={{ height: '20%' }}>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>Receive</h1>
+            <h1 style={{ fontWeight: 100 }}>Receive</h1>
           </div>
           <div style={{ width: '50%', display: 'inline', float: 'left', textAlign: 'center' }}>
-            <h1>{this.state.receiveAmount}</h1>
+            <h1 style={{ fontWeight: 100 }}>{this.state.receiveAmount}</h1>
           </div>
         </div>
       </Row>
