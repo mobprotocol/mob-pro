51af55b28c673b31b15330e438243b8d4594cdca
Sean Pollock <seanpollock3344@gmail.com>
2017-07-11 08:42:27 -0400

    cleaning up tokenA box

--- src/components/header/TokenA.js
+++ src/components/header/TokenA.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 export default class TokenA extends Component {
   render() {
+    const y = '-100%'
     return (
-      <h3>
-        <img src='./src/imgs/brave_logo.png'style={{ paddingRight: 10, paddingLeft: 10 }} />
-        BRAVE
+      <h3 style={{ position: 'relative', top: '50%' }}>
+        <img src='./src/imgs/brave_logo.png' style={{ paddingRight: 10, paddingLeft: 10 }} />
+        ETH
       </h3>
     )
   }
