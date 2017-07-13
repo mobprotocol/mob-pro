7e59bf31e86d6573d58ee71be8144545fffcce7b
Sean Pollock <seanpollock3344@gmail.com>
2017-07-13 12:07:37 -0400

    adding metamask modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { connect } from 'react-redux'
-import { Grid, Row } from 'react-bootstrap'
+import { Grid, Row, Modal } from 'react-bootstrap'
 import { Socket } from '../actions/index'
 import Header from './header/Header'
 import Content from './content/Content'
 import store from '../store'
 
 export default class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      show: true
+    }
+  }
   render() {
     return (
       <Provider store={store}>
-        <div style={{ fontFamily: 'Roboto' }}>
-          <Header />
-          <Content />
+        <div>
+          <div style={{ fontFamily: 'Roboto' }}>
+            <Header />
+            <Content />
+          </div>
+          <Modal show={this.state.show} style={{ maxHeight: 400, fontFamily: 'roboto' }}>
+            <Modal.Header>
+              <h3 style={{ fontWeight: 300 }}>GO DOWNLOAD METAMASK</h3>
+            </Modal.Header>
+            <Modal.Body>
+              
+            </Modal.Body>
+          </Modal>
         </div>
       </Provider>
     )
